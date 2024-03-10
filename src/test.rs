3d84b40398b82c73497e10115302aa7617da1ea9
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-09 22:26:59 -0500

    turned example into a test

--- src/test.rs
+++ src/test.rs
@@ -1,9 +1,31 @@
+use std::time::Duration;
+
+use tokio::time::sleep;
+
 use super::*;
 
 #[tokio::test]
-async fn root() {
+async fn serve_files() {
     tracing_subscriber::fmt::init();
-    tokio::spawn(server("localhost:8000"));
-    let http_client = httpc_test::new_client("http://localhost:8000").unwrap();
-    http_client.do_get("/").await.unwrap().print().await.unwrap();
+
+    tokio::spawn(server(DEFAULT_HOST_IP, DEFAULT_ROOT_DIR));
+    sleep(Duration::from_millis(100)).await;
+
+    let http_client = httpc_test::new_client(format!("http://{}", DEFAULT_HOST_IP)).unwrap();
+
+    const FILE_PATHS: &[&str] = &[
+        "/",
+        "/assets/duck_drink.gif",
+        "/assets/duck.jpg",
+        "/assets/many_ducks.gif",
+        "/assets/sleepy_duck.gif",
+    ];
+    for file_path in FILE_PATHS {
+        info!("trying to get {}", file_path);
+        let response = http_client.do_get(&file_path).await.expect("do_get failed");
+
+        response.print().await.expect("print failed");
+
+        assert!(response.status().is_success());
+    }
 }
