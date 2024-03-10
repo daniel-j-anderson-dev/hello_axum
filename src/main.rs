13b9fb197ac907e85aeefd3def04204974e98bcd
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-09 22:27:14 -0500

    use tower_http ServeDir

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,37 @@
+mod handlers;
 #[cfg(test)]
 mod test;
 
 use std::env;
 
-use axum::{
-    http::StatusCode,
-    routing::{get, post},
-    Json, Router,
-};
-use serde::{Deserialize, Serialize};
+use axum::Router;
 use tokio::net::TcpListener;
-use tracing::{debug, info};
+use tower_http::services::ServeDir;
+use tracing::{info, Level};
+
+const DEFAULT_HOST_IP: &str = "localhost:8080";
+const DEFAULT_ROOT_DIR: &str = "web";
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    tracing_subscriber::fmt::init();
-    let ip = env::args().nth(1).unwrap_or("localhost:8000".into());
-    server(&ip).await?;
+    tracing_subscriber::fmt().with_max_level(Level::TRACE).init();
+    
+    let host_ip = env::args().nth(1).unwrap_or(DEFAULT_HOST_IP.into());
+    let root_directory = env::args().nth(2).unwrap_or(DEFAULT_ROOT_DIR.into());
+    
+    info!("Listening on serving {} on {}", root_directory, host_ip);
+    
+    server(&host_ip, &root_directory).await?;
+
     return Ok(());
 }
 
-pub async fn server(ip: &str) ->  Result<(), std::io::Error> {
-    let listener = TcpListener::bind(ip).await?;
-    
-    let router = Router::new()
-        .route("/", get(root));
-    
-    info!("Listening on {}", ip);
-    
+pub async fn server(host_ip: &str, root_dir: &str) -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind(host_ip).await?;
+
+    let router = Router::new().nest_service("/", ServeDir::new(root_dir));
+
     axum::serve(listener, router).await?;
-    
-    return Ok(());
-}
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    info!("Hello from handle root!");
-    "Hello, World!"
+    return Ok(());
 }
