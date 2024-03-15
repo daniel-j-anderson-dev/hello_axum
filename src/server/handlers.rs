4ef042a5dfe3c3adf0e18160a2f6af651f6b4206
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-15 17:04:48 -0400

    Changed logging formatting

--- src/server/handlers.rs
+++ src/server/handlers.rs
@@ -7,7 +7,7 @@ use axum::{
     response::Response,
 };
 use tokio::{fs::File, io::AsyncReadExt};
-use tracing::{debug, info, warn};
+use tracing::{debug, error, info};
 
 /// Return the file data or a status code.
 /// - This function logs any status code returned with [warn]
@@ -17,13 +17,13 @@ pub async fn open_file(file_path: &str) -> Result<Vec<u8>, StatusCode> {
     File::open(&file_path)
         .await
         .map_err(|e| {
-            warn!("Failed to open {}: {}", file_path, e);
+            error!("Failed to open {}: {}", file_path, e);
             StatusCode::NOT_FOUND
         })?
         .read_to_end(&mut file_data)
         .await
         .map_err(|e| {
-            warn!("Failed to read {}: {}", file_path, e);
+            error!("Failed to read {}: {}", file_path, e);
             StatusCode::INTERNAL_SERVER_ERROR
         })?;
 
@@ -38,7 +38,7 @@ pub async fn serve_file(
 ) -> Result<Response, StatusCode> {
     let file_path = format!("{}/{}", root_dir, file_path);
 
-    info!("{} requested", file_path);
+    info!("File requested ({})", file_path);
 
     let file_data = open_file(&file_path).await?;
 
@@ -51,9 +51,9 @@ pub async fn serve_file(
 pub async fn root_index(
     State(root_dir): State<String>
 ) -> Result<Response, StatusCode> {
-    let file_path = format!("{}/../index.html", root_dir);
+    let file_path = format!("{}/index.html", root_dir);
 
-    info!("root requested: {}", file_path);
+    info!("Root index requested ({})", file_path);
 
     let file_data = open_file(&file_path).await?;
 
