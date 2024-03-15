e141387e21f36d7d4cdc45c1333f6c0a93ebabf6
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-15 15:52:57 -0400

    Refactored

--- src/server/handlers.rs
+++ src/server/handlers.rs
@@ -9,16 +9,9 @@ use axum::{
 use tokio::{fs::File, io::AsyncReadExt};
 use tracing::{debug, info, warn};
 
-#[debug_handler]
-/// Try to return a file in a response body. Use [tower_http::services::ServeDir] instead
-pub async fn serve_file(
-    State(root_dir): State<String>,
-    Path(file_path): Path<String>,
-) -> Result<Response, StatusCode> {
-    let file_path = format!("{}/{}", root_dir, file_path);
-
-    info!("{} requested", file_path);
-
+/// Return the file data or a status code.
+/// - This function logs any status code returned with [warn]
+pub async fn open_file(file_path: &str) -> Result<Vec<u8>, StatusCode> {
     let mut file_data = Vec::new();
 
     File::open(&file_path)
@@ -34,6 +27,21 @@ pub async fn serve_file(
             StatusCode::INTERNAL_SERVER_ERROR
         })?;
 
+    return Ok(file_data);
+}
+
+#[debug_handler]
+/// Try to return a file in a response body. Use [tower_http::services::ServeDir] instead
+pub async fn serve_file(
+    State(root_dir): State<String>,
+    Path(file_path): Path<String>,
+) -> Result<Response, StatusCode> {
+    let file_path = format!("{}/{}", root_dir, file_path);
+
+    info!("{} requested", file_path);
+
+    let file_data = open_file(&file_path).await?;
+
     let response = Response::new(file_data.into());
 
     return Ok(response);
@@ -47,20 +55,7 @@ pub async fn root_index(
 
     info!("root requested: {}", file_path);
 
-    let mut file_data = Vec::new();
-
-    File::open(&file_path)
-    .await
-    .map_err(|e| {
-        warn!("Failed to open {}: {}", file_path, e);
-        StatusCode::NOT_FOUND
-    })?
-    .read_to_end(&mut file_data)
-    .await
-    .map_err(|e| {
-        warn!("Failed to read {}: {}", file_path, e);
-        StatusCode::INTERNAL_SERVER_ERROR
-    })?;
+    let file_data = open_file(&file_path).await?;
 
     let response = Response::new(file_data.into());
 
