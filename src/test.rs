fb05ae4e7585f75a0c0910000e19f228d2e23f42
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-15 15:42:09 -0400

    create tests for the different servers

--- src/test.rs
+++ src/test.rs
@@ -1,31 +1,72 @@
-use std::time::Duration;
+use std::{sync::Once, time::Duration};
 
 use tokio::time::sleep;
+use tracing::{error, trace, warn};
 
 use super::*;
 
+const FILE_PATHS: &[&str] = &[
+    "/",
+    "/assets/duck_drink.gif",
+    "/assets/duck.jpg",
+    "/assets/many_ducks.gif",
+    "/assets/sleepy_duck.gif",
+];
+
+pub fn initialize_stdout_subscriber() {
+    static INIT: Once = Once::new();
+
+    INIT.call_once(|| {
+        tracing_subscriber::fmt::Subscriber::builder()
+            .with_writer(std::io::stdout)
+            .init();
+    });
+}
+
 #[tokio::test]
 async fn serve_files() {
-    tracing_subscriber::fmt::init();
+    initialize_stdout_subscriber();
 
-    tokio::spawn(server(DEFAULT_HOST_IP, DEFAULT_ROOT_DIR));
+    let root_dir = format!("{}/assets", DEFAULT_ROOT_DIR);
+
+    tokio::spawn(server::host_files_with_index(DEFAULT_HOST_IP, root_dir.clone()));
     sleep(Duration::from_millis(100)).await;
 
     let http_client = httpc_test::new_client(format!("http://{}", DEFAULT_HOST_IP)).unwrap();
 
-    const FILE_PATHS: &[&str] = &[
-        "/",
-        "/assets/duck_drink.gif",
-        "/assets/duck.jpg",
-        "/assets/many_ducks.gif",
-        "/assets/sleepy_duck.gif",
-    ];
     for file_path in FILE_PATHS {
         info!("trying to get {}", file_path);
         let response = http_client.do_get(&file_path).await.expect("do_get failed");
 
-        response.print().await.expect("print failed");
+        if let Err(e) = response.print().await {
+            warn!("failed to print response: {}", e);
+        }
 
-        assert!(response.status().is_success());
+        if !response.status().is_success() {
+            error!("Response failed!");
+        }
+    }
+}
+
+#[tokio::test]
+async fn tower_serve_dir() {
+    initialize_stdout_subscriber();
+
+    tokio::spawn(server::tower_serve_dir(DEFAULT_HOST_IP, DEFAULT_ROOT_DIR));
+    sleep(Duration::from_millis(100)).await;
+
+    let http_client = httpc_test::new_client(format!("http://{}", DEFAULT_HOST_IP)).unwrap();
+
+    for file_path in FILE_PATHS {
+        info!("trying to get {}", file_path);
+        let response = http_client.do_get(&file_path).await.expect("do_get failed");
+
+        if let Err(e) = response.print().await {
+            warn!("failed to print response: {}", e);
+        }
+
+        if !response.status().is_success() {
+            error!("Response failed!");
+        }
     }
 }
