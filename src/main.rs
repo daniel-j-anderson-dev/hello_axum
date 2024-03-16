26e00d5194aee3a56d0c7352b19e442b5e6ffae7
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-16 00:29:03 -0400

    SocketAddr not String

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use hello_axum::{servers, DEFAULT_HOST_IP, DEFAULT_ROOT_DIR};
+use hello_axum::{servers, DEFAULT_HOST_ADDRESS, DEFAULT_ROOT_DIR};
 use tracing::{info, Level};
 
 #[tokio::main]
@@ -9,7 +9,7 @@ async fn main() -> Result<(), std::io::Error> {
         .with_max_level(Level::DEBUG)
         .init();
 
-    let host_ip = env::args().nth(1).unwrap_or(DEFAULT_HOST_IP.into());
+    let host_ip = env::args().nth(1).unwrap_or(DEFAULT_HOST_ADDRESS.to_string());
     let root_directory = env::args().nth(2).unwrap_or(DEFAULT_ROOT_DIR.into());
 
     info!("Listening on serving {} on {}", root_directory, host_ip);
