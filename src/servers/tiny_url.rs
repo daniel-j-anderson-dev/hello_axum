af88e5098972265128eb6bbadf5cc3400aa62c0e
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-16 12:36:45 -0400

    Added final endpoint handler

--- src/servers/tiny_url.rs
+++ src/servers/tiny_url.rs
@@ -1,15 +1,15 @@
 use std::{
     collections::{hash_map::Entry, HashMap},
-    net::{IpAddr, Ipv4Addr, SocketAddr},
+    net::SocketAddr,
     sync::{Arc, Mutex},
 };
 
-use axum::{body::Body, debug_handler, extract::State, http::StatusCode, response::Response, Json};
+use axum::{debug_handler, extract::{Path, State}, http::StatusCode, response::Response, Json};
 use serde::Deserialize;
-use tracing::{debug, error, Level};
+use tracing::{debug, error};
 use url::Url;
 
-use super::*;
+use crate::{servers::*, DEFAULT_HOST_ADDRESS};
 
 /// A REST API server to minimize urls
 /// # Endpoints
@@ -30,9 +30,6 @@ pub struct TinyUrlServer {
     long_to_tiny_map: HashMap<Url, Url>,
 }
 impl TinyUrlServer {
-    pub const DEFAULT_HOST_ADDRESS: SocketAddr =
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
-
     pub fn from_env_or_default() -> Self {
         return Self {
             host_address: std::env::args()
@@ -40,7 +37,7 @@ impl TinyUrlServer {
                 .and_then(|s| {
                     return s.parse().ok();
                 })
-                .unwrap_or(Self::DEFAULT_HOST_ADDRESS),
+                .unwrap_or(DEFAULT_HOST_ADDRESS),
             long_to_tiny_map: HashMap::new(),
         };
     }
@@ -62,19 +59,17 @@ impl TinyUrlServer {
         });
     }
 
-    async fn listener(&self) -> Result<TcpListener, std::io::Error> {
-        return TcpListener::bind(self.host_address).await;
-    }
-
-    fn router(self) -> Router {
-        let long_to_tiny_map = Arc::new(Mutex::new(self.long_to_tiny_map));
-        return Router::new()
-            .route("/create-url", post(create_url))
-            .with_state(long_to_tiny_map);
-    }
-
     pub async fn run(self) -> Result<(), std::io::Error> {
-        axum::serve(self.listener().await?, self.router()).await?;
+        let listener = TcpListener::bind(self.host_address).await?;
+
+        let long_to_tiny_map = Arc::new(Mutex::new(self.long_to_tiny_map));
+
+        let router = Router::new()
+            .route("/create-url", post(create_url))
+            .route("/*tiny-url", get(redirect_tiny_url))
+            .with_state(long_to_tiny_map);
+
+        axum::serve(listener, router).await?;
 
         return Ok(());
     }
@@ -82,7 +77,7 @@ impl TinyUrlServer {
 impl Default for TinyUrlServer {
     fn default() -> Self {
         return Self {
-            host_address: Self::DEFAULT_HOST_ADDRESS,
+            host_address: DEFAULT_HOST_ADDRESS,
             long_to_tiny_map: HashMap::new(),
         };
     }
@@ -104,9 +99,11 @@ pub struct Params {
 #[debug_handler]
 pub async fn create_url(
     State(long_to_tiny_map): State<TinyUrlMap>,
-    Json(Params{ long_url }): Json<Params>,
-) -> Result<StatusCode, StatusCode> {
-    debug!("POST /create-url ({})", long_url);
+    Json(Params { long_url }): Json<Params>,
+) -> Result<Response<String>, StatusCode> {
+    debug!("POST /create-url {}", long_url);
+
+    let tiny_url = minify_url(&long_url);
 
     let mut long_to_tiny_map = match long_to_tiny_map.try_lock() {
         Ok(lock) => lock,
@@ -118,16 +115,44 @@ pub async fn create_url(
 
     match long_to_tiny_map.entry(long_url) {
         Entry::Occupied(o_e) => {
-            debug!("Entry already made \"{}\": \"{}\"", o_e.key(), o_e.get());
+            debug!("Entry already made shortened url: \"{}\"", o_e.get());
         }
         Entry::Vacant(v_e) => {
-            debug!("Creating tiny-url");
-            let tiny_url = minify_url(v_e.key());
-
             debug!("Added {{ \"{}\": \"{}\" }}", v_e.key(), tiny_url);
-            v_e.insert(tiny_url);
+            v_e.insert(tiny_url.clone());
         }
     };
 
-    return Ok(StatusCode::CREATED);
+    let mut response = Response::new(tiny_url.to_string());
+    *response.status_mut() = StatusCode::CREATED;
+
+    debug!("{:?}", response);
+
+    return Ok(response);
+}
+
+#[debug_handler]
+pub async fn redirect_tiny_url(
+    State(long_to_tiny_map): State<TinyUrlMap>,
+    Path(tiny_url): Path<String>,
+) -> Result<Response<String>, StatusCode> {
+    let long_to_tiny_map = match long_to_tiny_map.try_lock() {
+        Ok(lock) => lock,
+        Err(e) => {
+            error!("Failed to to lock big_to_tiny_map: {}", e);
+            return Err(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+
+    let long_url = long_to_tiny_map
+        .iter()
+        .find_map(|(k, v)| if v.to_string() == tiny_url {Some(k)} else {None})
+        .ok_or(StatusCode::NOT_FOUND)?;
+
+    let mut response = Response::new(long_url.to_string());
+    *response.status_mut() = StatusCode::PERMANENT_REDIRECT;
+
+    debug!("{:?}", response);
+
+    return Ok(response);
 }
