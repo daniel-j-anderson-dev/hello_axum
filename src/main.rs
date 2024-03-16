d9d1bda38a38c7f16503acab37667fe4586b821a
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-16 12:36:02 -0400

    use tinyurlserver

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use hello_axum::{servers, DEFAULT_HOST_ADDRESS, DEFAULT_ROOT_DIR};
+use hello_axum::{servers::{self, tiny_url::TinyUrlServer}, DEFAULT_HOST_ADDRESS, DEFAULT_ROOT_DIR};
 use tracing::{info, Level};
 
 #[tokio::main]
@@ -9,12 +9,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_max_level(Level::DEBUG)
         .init();
 
-    let host_addr = env::args().nth(1).unwrap_or(DEFAULT_HOST_ADDRESS.to_string()).parse()?;
+    let host_addr = env::args()
+        .nth(1)
+        .unwrap_or(DEFAULT_HOST_ADDRESS.to_string());
+        // .parse()?;
     let root_directory = env::args().nth(2).unwrap_or(DEFAULT_ROOT_DIR.into());
 
-    info!("Listening on serving {} on {}", root_directory, host_addr);
+    info!("Listening on {}", host_addr);
 
-    servers::host_files_with_index(host_addr, root_directory).await?;
+    TinyUrlServer::new(&host_addr)?.run().await?;
+    // servers::host_files_with_index(host_addr, root_directory).await?;
     // server::tower_serve_dir(&host_ip, &root_directory).await?;
 
     return Ok(());
