7611c31ce0c6b0e8f1075094273ee5ed22504db1
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-16 00:27:10 -0400

    made DEFAULT_HOST_ADDRESS a SocketAddr

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 #[cfg(test)]
 mod test;
 
 pub mod servers;
 
-pub const DEFAULT_HOST_IP: &str = "localhost:8080";
+pub const DEFAULT_HOST_ADDRESS: SocketAddr =
+    SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
 pub const DEFAULT_ROOT_DIR: &str = "web";
