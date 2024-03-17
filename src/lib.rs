37bf6a80f12ea721ca12903c40dbe351bcac6255
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-17 19:58:23 -0400

    Moved tracing init helper to root

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
+use tracing::{debug, Level};
+
 #[cfg(test)]
 mod test;
 
 pub mod servers;
 
-pub const DEFAULT_HOST_ADDRESS: SocketAddr =
+pub const LOCAL_HOST_8080: SocketAddr =
     SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
 pub const DEFAULT_ROOT_DIR: &str = "web";
+pub const EXAMPLE_URL: &str = "https://www.euclideanspace.com/maths/geometry/trig/functions/index.htm";
+
+pub fn initialize_stdout_subscriber(level: Level) {
+    if let Err(e) = tracing_subscriber::fmt::Subscriber::builder()
+        .with_max_level(level)
+        .with_writer(std::io::stdout)
+        .try_init()
+    {
+        debug!("Failed to initialize subscriber: {}", e);
+    }
+}
