6f7e06c9288920dadfca7b0a351f565ff72a81f4
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-03-17 19:59:37 -0400

    remove unused import

--- examples/tiny_url.rs
+++ examples/tiny_url.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use tracing::{info, trace, Level};
+use tracing::{info, Level};
 
 use hello_axum::{initialize_stdout_subscriber, servers::tiny_url::TinyUrlServer, LOCAL_HOST_8080};
 
